59d11157004c060009f391037db3067fdc05d77b
Gouranga Satapathy <pulusatapathy@gmail.com>
2023-12-27 22:36:09 +0530

    added modules Repository and corsConfig refactored and blocklist.txt updated

--- src/main/java/com/ultimateScrape/serviceRegistry/serviceRegistry/AppConfig.java
+++ src/main/java/com/ultimateScrape/serviceRegistry/serviceRegistry/AppConfig.java
@@ -1,27 +1,9 @@
 package com.ultimateScrape.serviceRegistry.serviceRegistry;
 
-import org.springframework.context.annotation.Bean;
+import org.example.ModuleConfigurationApp;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
+import org.springframework.context.annotation.Import;
 
 @Configuration
-public class AppConfig {
-
-    @Bean
-    public CorsWebFilter corsWebFilter() {
-        CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
-        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        corsConfig.setMaxAge(3600L);
-        corsConfig.addAllowedHeader("*");
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig);
-
-        return new CorsWebFilter(source);
-    }
-}
+@Import({ModuleConfigurationApp.class})
+public class AppConfig { }
