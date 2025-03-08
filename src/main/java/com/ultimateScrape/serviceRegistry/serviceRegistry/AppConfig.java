321876fc9e1f2d41bdc8309f4e9192382bf88797
Gouranga Satapathy <pulusatapathy@gmail.com>
2025-03-08 14:04:56 +0530

    update all

--- src/main/java/com/ultimateScrape/serviceRegistry/serviceRegistry/AppConfig.java
+++ src/main/java/com/ultimateScrape/serviceRegistry/serviceRegistry/AppConfig.java
@@ -1,9 +1,24 @@
 package com.ultimateScrape.serviceRegistry.serviceRegistry;
 
 import org.example.ModuleConfigurationApp;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 @Import({ModuleConfigurationApp.class})
-public class AppConfig { }
+public class AppConfig {
+
+    @Value("${allowed.origins}")
+    private String allowedOrigins;
+
+    @Bean
+    public List<String> allowedOrigins() {
+        return Arrays.asList(allowedOrigins.split(","));
+    }
+
+}
